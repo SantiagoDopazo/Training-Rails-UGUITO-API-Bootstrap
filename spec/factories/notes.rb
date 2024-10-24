67012c9ac9e1c830757a56311af7b806cc8b95e7
SantiagoDopazo <dopazosantiago18@gmail.com>
2024-10-24 14:23:32 -0300

    change user possition in FactoryBot

--- spec/factories/notes.rb
+++ spec/factories/notes.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :note do
-    user
     title { Faker::Book.title }
     content { Faker::Lorem.paragraph }
     note_type { Note.note_types.keys.sample }
+    user
   end
 end
