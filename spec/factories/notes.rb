c591e3f1779c0d3aac449d6a828bffdc93f01f83
Santiago Agustin Dopazo <dopazosantiago18@gmail.com>
2024-10-24 16:05:12 -0300

    feat: [TR-590] Added Note Module
    * feat implemention of a model Note so users can create multiple of them about the books they have
    
    * delete test to respect objective of the actual card
    
    * [200~fix(deps): restore Gemfile.lock to match master
    
    * fix(deps): restore Gemfile.lock to match master
    
    * fix: changed note_type to enum and added null constraints
    
    * fix: changed note FactoryBot to use user FactoryBot

---
+++ spec/factories/notes.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :note do
+    title { Faker::Book.title }
+    content { Faker::Lorem.paragraph }
+    note_type { Note.note_types.keys.sample }
+    user
+  end
+end
---
+++ spec/models/note_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe Note, type: :model do
+  pending "ad some examples to (or delete) #{__FILE__}"
+end
