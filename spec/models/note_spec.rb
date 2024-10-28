d7fa73fad5968673abc4775290ad2f368375040c
SantiagoDopazo <dopazosantiago18@gmail.com>
2024-10-28 15:23:47 -0300

    fix: change names of test to be shorter, position and change validation describe to be #save

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,35 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
-  subject do
-    create(:note)
-  end
+  subject(:note) { build(:note, note_type: :review, content: content, user: user) }
+
+  let(:content) { Faker::Lorem.words(number: words) }
 
   let(:user) { create(:user, utility: utility) }
-  let(:note) { build(:note, note_type: :review, content: Faker::Lorem.words(number: content), user: user) }
 
-  describe 'validations' do
-    context 'when validating presence' do
-      %i[user_id title content note_type].each do |value|
-        it { is_expected.to validate_presence_of(value) }
-      end
-    end
+  it { is_expected.to belong_to(:user) }
 
+  it 'has a valid factory' do
+    expect(subject).to be_valid
+  end
+
+  %i[user_id title content note_type].each do |value|
+    it { is_expected.to validate_presence_of(value) }
+  end
+
+  describe '#save!' do
     context 'when user has NorthUtility' do
       let(:utility) { create(:north_utility) }
 
       context 'with valid word count' do
-        let(:content) { rand(0..50) }
+        let(:words) { rand(0..50) }
 
-        it 'is valid if review word count is in NorthUtility limit' do
-          expect(note).to be_valid
+        it 'create note succesfully' do
+          expect(note.save!).to be_truthy
         end
       end
 
       context 'with invalid word count' do
         let(:content) { rand(51..99) }
 
-        it 'is invalid if review word count exceeds NorthUtility limit' do
+        it 'returns ActiveRecord::RecordInvalid' do
           expect { note.save! }.to raise_error(ActiveRecord::RecordInvalid)
         end
       end
@@ -41,15 +44,15 @@ RSpec.describe Note, type: :model do
       context 'with valid word count' do
         let(:content) { rand(0..60) }
 
-        it 'is valid if review word count is in SouthUtility limit' do
-          expect(note).to be_valid
+        it 'create note succesfully' do
+          expect(note.save!).to be_truthy
         end
       end
 
       context 'with invalid word count' do
         let(:content) { rand(61..120) }
 
-        it 'is invalid if review word count exceeds SouthUtility limit' do
+        it 'returns ActiveRecord::RecordInvalid' do
           expect { note.save! }.to raise_error(ActiveRecord::RecordInvalid)
         end
       end
@@ -70,7 +73,7 @@ RSpec.describe Note, type: :model do
     context 'when content contains one word' do
       let(:content) { 'Hello' }
 
-      it 'returns 1' do
+      it 'returns the correct word count' do
         expect(note.word_count).to eq(1)
       end
     end
@@ -86,7 +89,7 @@ RSpec.describe Note, type: :model do
     context 'when content contains multiple spaces' do
       let(:content) { 'Hello    world' }
 
-      it 'counts words correctly, ignoring extra spaces' do
+      it 'returns the correct word count' do
         expect(note.word_count).to eq(2)
       end
     end
@@ -99,7 +102,7 @@ RSpec.describe Note, type: :model do
       context 'with short content' do
         let(:content) { rand(0..utility.short_content) }
 
-        it 'returns "short" if word count is equal to or less than short_content' do
+        it 'returns "short"' do
           expect(note.content_length).to eq('short')
         end
       end
@@ -107,7 +110,7 @@ RSpec.describe Note, type: :model do
       context 'with medium content' do
         let(:content) { rand(utility.short_content + 1..utility.medium_content) }
 
-        it 'returns "medium" if word count is greater than short_content and equal to or less than medium_content' do
+        it 'returns "medium"' do
           expect(note.content_length).to eq('medium')
         end
       end
@@ -115,7 +118,7 @@ RSpec.describe Note, type: :model do
       context 'with long content' do
         let(:content) { rand(utility.medium_content + 1..999) }
 
-        it 'returns "long" if word count is greater than medium_content' do
+        it 'returns "long"' do
           expect(note.content_length).to eq('long')
         end
       end
@@ -127,7 +130,7 @@ RSpec.describe Note, type: :model do
       context 'with short content' do
         let(:content) { rand(0..utility.short_content) }
 
-        it 'returns "short" if word count is equal to or less than short_content' do
+        it 'returns "short"' do
           expect(note.content_length).to eq('short')
         end
       end
@@ -135,7 +138,7 @@ RSpec.describe Note, type: :model do
       context 'with medium content' do
         let(:content) { rand(utility.short_content + 1..utility.medium_content) }
 
-        it 'returns "medium" if word count is greater than short_content and equal to or less than medium_content' do
+        it 'returns "medium"' do
           expect(note.content_length).to eq('medium')
         end
       end
@@ -143,16 +146,10 @@ RSpec.describe Note, type: :model do
       context 'with long content' do
         let(:content) { rand(utility.medium_content + 1..999) }
 
-        it 'returns "long" if word count is greater than medium_content' do
+        it 'returns "long"' do
           expect(note.content_length).to eq('long')
         end
       end
     end
-  end
-
-  it { is_expected.to belong_to(:user) }
-
-  it 'has a valid factory' do
-    expect(subject).to be_valid
   end
 end
