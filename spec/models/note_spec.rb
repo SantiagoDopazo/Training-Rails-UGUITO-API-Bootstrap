921777b190f54c48a658578349437ae3fbec1ec1
SantiagoDopazo <dopazosantiago18@gmail.com>
2024-10-31 18:24:05 -0300

    upgrading tests to note model

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Note, type: :model do
   let(:note) { build(:note, user: user) }
-  let(:note_with_attributes) { build(:note, note_type: :review, content: Faker::Lorem.words(number: content), user: user) }
-  let(:utility) { create(:north_utility) }
+  let(:note_with_attributes) { build(:note, note_type: :review, content: Faker::Lorem.sentence(word_count: number), user: user) }
+  let(:utility) { create(%i[north_utility south_utility].sample) }
   let(:user) { create(:user, utility: utility) }
 
   it { is_expected.to belong_to(:user) }
@@ -16,139 +16,63 @@ RSpec.describe Note, type: :model do
     it { is_expected.to validate_presence_of(value) }
   end
 
-  describe '#save!' do
-    context 'when user has NorthUtility' do
-      let(:utility) { create(:north_utility) }
+  shared_examples 'returns content length' do |length|
+    it "returns #{length}" do
+      expect(note_with_attributes.content_length).to eq(length)
+    end
+  end
 
-      context 'with valid word count' do
-        let(:content) { rand(0..50) }
+  describe '#validate_review_word_limit' do
+    let(:user) { create(:user, utility: utility) }
 
-        it 'create note succesfully' do
-          expect(note_with_attributes.save!).to be_truthy
-        end
-      end
+    context 'with valid word count' do
+      let(:number) { Faker::Number.between(from: 0, to: utility.short_content) }
 
-      context 'with invalid word count' do
-        let(:content) { rand(51..99) }
-
-        it 'returns ActiveRecord::RecordInvalid' do
-          expect { note_with_attributes.save! }.to raise_error(ActiveRecord::RecordInvalid)
-        end
+      it 'does not have errors' do
+        note_with_attributes.save
+        expect(note_with_attributes.errors.count).to eq(0)
       end
     end
 
-    context 'when user has SouthUtility' do
-      let(:utility) { create(:south_utility) }
+    context 'with invalid word count' do
+      let(:number) { Faker::Number.between(from: utility.short_content + 1, to: 999) }
 
-      context 'with valid word count' do
-        let(:content) { rand(0..60) }
-
-        it 'create note succesfully' do
-          expect(note_with_attributes.save!).to be_truthy
-        end
-      end
-
-      context 'with invalid word count' do
-        let(:content) { rand(61..120) }
-
-        it 'returns ActiveRecord::RecordInvalid' do
-          expect { note_with_attributes.save! }.to raise_error(ActiveRecord::RecordInvalid)
-        end
+      it 'has errors' do
+        note_with_attributes.save
+        expect(note_with_attributes.errors.count).to eq(1)
       end
     end
   end
 
   describe '#word_count' do
-    let(:note) { build(:note, content: content) }
+    let(:expected_word_count) { note_with_attributes.content.split.size }
 
-    context 'when content is nil' do
-      let(:content) { nil }
-
-      it 'returns 0' do
-        expect(note.word_count).to eq(0)
-      end
-    end
-
-    context 'when content contains one word' do
-      let(:content) { 'Hello' }
+    context 'when content is random' do
+      let(:number) { Faker::Number.between(from: 0, to: 50) }
 
       it 'returns the correct word count' do
-        expect(note.word_count).to eq(1)
-      end
-    end
-
-    context 'when content contains multiple words' do
-      let(:content) { 'Hello world! im doing test in ruby.' }
-
-      it 'returns the correct word count' do
-        expect(note.word_count).to eq(7)
-      end
-    end
-
-    context 'when content contains multiple spaces' do
-      let(:content) { 'Hello    world' }
-
-      it 'returns the correct word count' do
-        expect(note.word_count).to eq(2)
+        expect(note_with_attributes.word_count).to eq(expected_word_count)
       end
     end
   end
 
   describe '#content_length' do
-    context 'when user has NorthUtility' do
-      let(:utility) { create(:north_utility) }
+    context 'with short content' do
+      let(:number) { Faker::Number.between(from: 0, to: utility.short_content) }
 
-      context 'with short content' do
-        let(:content) { rand(0..utility.short_content) }
-
-        it 'returns "short"' do
-          expect(note_with_attributes.content_length).to eq('short')
-        end
-      end
-
-      context 'with medium content' do
-        let(:content) { rand(utility.short_content + 1..utility.medium_content) }
-
-        it 'returns "medium"' do
-          expect(note_with_attributes.content_length).to eq('medium')
-        end
-      end
-
-      context 'with long content' do
-        let(:content) { rand(utility.medium_content + 1..999) }
-
-        it 'returns "long"' do
-          expect(note_with_attributes.content_length).to eq('long')
-        end
-      end
+      it_behaves_like 'returns content length', 'short'
     end
 
-    context 'when user has SouthUtility' do
-      let(:utility) { create(:south_utility) }
+    context 'with medium content' do
+      let(:number) { Faker::Number.between(from: utility.short_content + 1, to: utility.medium_content) }
 
-      context 'with short content' do
-        let(:content) { rand(0..utility.short_content) }
+      it_behaves_like 'returns content length', 'medium'
+    end
 
-        it 'returns "short"' do
-          expect(note_with_attributes.content_length).to eq('short')
-        end
-      end
+    context 'with long content' do
+      let(:number) { Faker::Number.between(from: utility.medium_content + 1, to: 999) }
 
-      context 'with medium content' do
-        let(:content) { rand(utility.short_content + 1..utility.medium_content) }
-
-        it 'returns "medium"' do
-          expect(note_with_attributes.content_length).to eq('medium')
-        end
-      end
-
-      context 'with long content' do
-        let(:content) { rand(utility.medium_content + 1..999) }
-
-        it 'returns "long"' do
-          expect(note_with_attributes.content_length).to eq('long')
-        end
-      end
+      it_behaves_like 'returns content length', 'long'
     end
   end
 end
