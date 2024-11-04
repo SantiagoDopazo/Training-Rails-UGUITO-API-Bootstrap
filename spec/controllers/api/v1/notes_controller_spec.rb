bc6138d4221680ebdc18b6363ec73c0e161030b2
SantiagoDopazo <dopazosantiago18@gmail.com>
2024-11-04 14:34:31 -0300

    feat: added context for valid and invalid note_types

--- spec/controllers/api/v1/notes_controller_spec.rb
+++ spec/controllers/api/v1/notes_controller_spec.rb
@@ -1,19 +1,20 @@
 require 'rails_helper'
 
-describe Api::V1::NotesController, type: :controller do
-  shared_examples 'successfully response' do
-    let(:expected) do
-      ActiveModel::Serializer::CollectionSerializer.new(notes_expected,
-                                                        serializer: IndexNoteSerializer).to_json
-    end
-    it 'responds with the expected notes json' do
-      expect(response_body.to_json).to eq(expected)
-    end
-
-    it 'responds with 200 status' do
-      expect(response).to have_http_status(:ok)
-    end
+shared_examples 'successfully response' do
+  let(:expected) do
+    ActiveModel::Serializer::CollectionSerializer.new(notes_expected,
+                                                      serializer: IndexNoteSerializer).to_json
   end
+  it 'responds with the expected notes json' do
+    expect(response_body.to_json).to eq(expected)
+  end
+
+  it 'responds with 200 status' do
+    expect(response).to have_http_status(:ok)
+  end
+end
+
+describe Api::V1::NotesController, type: :controller do 
   describe 'GET #index' do
     let(:user_notes) { create_list(:note, 3, user: user) }
 
@@ -53,9 +54,23 @@ describe Api::V1::NotesController, type: :controller do
         let!(:notes_custom) { create_list(:note, 2, user: user, note_type: note_type) }
         let(:notes_expected) { notes_custom }
 
-        before { get :index, params: { note_type: note_type } }
+        context 'when note_type is valid' do
+          before { get :index, params: { note_type: note_type } }
 
-        include_examples 'successfully response'
+          include_examples 'successfully response'
+        end
+
+        context 'when note_type is invalid' do
+          let(:invalid_note_type) { 'invalid' }
+
+          before do
+            get :index, params: { note_type: invalid_note_type }
+          end
+
+          it 'returns an unprocessable entity status' do
+            expect(response).to have_http_status(:unprocessable_entity)
+          end
+        end
       end
     end
 
