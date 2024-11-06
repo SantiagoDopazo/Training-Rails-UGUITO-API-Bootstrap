2fa5c5275cf5d77b308726bc464295af58b2e39f
Santiago Agustin Dopazo <dopazosantiago18@gmail.com>
2024-11-06 17:31:15 -0300

    feat: [TR-592] Implementing Tests To Note Model (#4)
    * feat implemention of a model Note so users can create multiple of them about the books they have
    
    * delete test to respect objective of the actual card
    
    * [200~fix(deps): restore Gemfile.lock to match master
    
    * fix(deps): restore Gemfile.lock to match master
    
    * fix: added method in  utility to return the content length of a note using Constant Hashes in each Utility
    
    * fix: changed note_type to enum and added null constraints
    
    * feat: change logic of content_length so its more delegate in Note model, added English and Spanish errors types
    
    * fix: added first argument in validate_review_word_limit method to fix no method error
    
    * feat: first test added
    
    * delete bybugs
    
    * upgrading tests and adding test to validate content_length methods
    
    * fix: content_length method to be more ruby style
    
    * fix: added last line in es.yml archive
    
    * feat: added more test for the content_length method
    
    * feat: added word_count test
    
    * change user possition in FactoryBot
    
    * fix: change names of test to be shorter, position and change validation describe to be #save
    
    * fix: change lets so test can use them correctly
    
    * fix: word_count to be more ruby if content is nil
    
    * upgrading tests to note model
    
    * change number between in word_count
    
    * fix changes requested in PR
    
    * fix: shared examples move outside Rspec describe
    
    * fix: more changes to upgrade tests
    
    * fix: move lets in the only test they used them and addes subject
    
    * remove note_type in content_length
    
    * using note save in the before block so i dont have to repeat in each it

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,5 +1,81 @@
 require 'rails_helper'
 
+shared_examples 'a content_length response' do |length|
+  it "returns #{length}" do
+    expect(note_with_attributes.content_length).to eq(length)
+  end
+end
+
 RSpec.describe Note, type: :model do
-  pending "ad some examples to (or delete) #{__FILE__}"
+  subject(:note) { create(:note) }
+
+  it { is_expected.to belong_to(:user) }
+
+  it 'has a valid factory' do
+    expect(subject).to be_valid
+  end
+
+  %i[user_id title content note_type].each do |value|
+    it { is_expected.to validate_presence_of(value) }
+  end
+
+  describe '#validate_review_word_limit' do
+    let(:utility) { create(%i[north_utility south_utility].sample) }
+    let(:user) { create(:user, utility: utility) }
+    let(:review_note) { build(:note, note_type: :review, user: user) }
+
+    before do
+      allow(review_note).to receive(:content_length).and_return(content_length)
+      review_note.save
+    end
+
+    context 'when content length is short' do
+      let(:content_length) { 'short' }
+
+      it 'does not have errors' do
+        expect(review_note.errors.count).to eq(0)
+      end
+    end
+
+    context 'when content length is not short' do
+      let(:content_length) { %w[medium long].sample }
+
+      it 'has errors' do
+        expect(review_note.errors.count).to eq(1)
+      end
+    end
+  end
+
+  describe '#word_count' do
+    let(:note_with_attributes) { build(:note, content: Faker::Lorem.sentence(word_count: words_number)) }
+    let(:words_number) { Faker::Number.between(from: 0, to: 999) }
+
+    it 'returns the correct word count' do
+      expect(note_with_attributes.word_count).to eq(words_number)
+    end
+  end
+
+  describe '#content_length' do
+    let(:utility) { create(%i[north_utility south_utility].sample) }
+    let(:user) { create(:user, utility: utility) }
+    let(:note_with_attributes) { build(:note, content: Faker::Lorem.sentence(word_count: words_number), user: user) }
+
+    context 'with short content' do
+      let(:words_number) { Faker::Number.between(from: 0, to: utility.short_content) }
+
+      it_behaves_like 'a content_length response', 'short'
+    end
+
+    context 'with medium content' do
+      let(:words_number) { Faker::Number.between(from: utility.short_content + 1, to: utility.medium_content) }
+
+      it_behaves_like 'a content_length response', 'medium'
+    end
+
+    context 'with long content' do
+      let(:words_number) { Faker::Number.between(from: utility.medium_content + 1, to: 999) }
+
+      it_behaves_like 'a content_length response', 'long'
+    end
+  end
 end
