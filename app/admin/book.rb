1e05cc30f4abb1a8ea0e1acaf6b3fcc2b045ffc6
Santiago Agustin Dopazo <dopazosantiago18@gmail.com>
2024-11-15 11:04:37 -0300

    feat: [TR-601] Active Admin Books View (#10)
    * feat implemention of a model Note so users can create multiple of them about the books they have
    
    * delete test to respect objective of the actual card
    
    * [200~fix(deps): restore Gemfile.lock to match master
    
    * fix(deps): restore Gemfile.lock to match master
    
    * fix: added method in  utility to return the content length of a note using Constant Hashes in each Utility
    
    * fix: changed note_type to enum and added null constraints
    
    * feat: change logic of content_length so its more delegate in Note model, added English and Spanish errors types
    
    * fix: added first argument in validate_review_word_limit method to fix no method error
    
    * feat: first test added
    
    * delete bybugs
    
    * upgrading tests and adding test to validate content_length methods
    
    * fix: content_length method to be more ruby style
    
    * fix: added last line in es.yml archive
    
    * feat: added more test for the content_length method
    
    * feat: added word_count test
    
    * change user possition in FactoryBot
    
    * feat: added show method with pagination
    
    * fix: change notes_filtered method to respect line length
    
    * feat: added show method
    
    * fix: wrong json in index and replace doble quotes with single quotes
    
    * feat: add a before_action to authenticate user before using index and show methods
    
    * feat: describe to index test
    
    * feat: test for show and index, added shared_examples to success responses
    
    * delete notes spec from request
    
    * rescue exceptiosn from invalid notes
    
    * finally controller rescue all posible exceptions
    
    * added test to note controller create service
    
    * feat: added routes for async test, adde worker and added index_async method
    
    * feat: added worker for index async request
    
    * delete bybugs
    
    * feat: mapper added to Response north
    
    * feat: working mappers for north and south endpoints
    
    * feat: test for index asyn service
    
    * feat: added view for books in active admin
    
    * feat: generated cassettes for RetrieveNotesWorker
    
    * feat: added test for worker
    
    * fix: show user email to create books and generate comments for routes
    
    * delete test file from 600
    
    * es and en gem file fixed

---
+++ app/admin/book.rb
@@ -0,0 +1,55 @@
+ActiveAdmin.register Book do
+  includes :utility, :user
+
+  permit_params %i[utility_id user_id genre author image title publisher year]
+
+  filter :genre
+  filter :author
+  filter :title
+  filter :publisher
+  filter :year
+  filter :created_at
+
+  index do
+    selectable_column
+    id_column
+    column :user
+    column :utility
+    column :genre
+    column :author
+    column :image
+    column :title
+    column :publisher
+    column :year
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :utility
+      row :user
+      row :genre
+      row :author
+      row :image
+      row :title
+      row :publisher
+      row :year
+      row :created_at
+      row :updated_at
+    end
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :utility
+      f.input :user, as: :select, collection: User.all.map { |user| [user.email.to_s, user.id] }
+      f.input :genre
+      f.input :author
+      f.input :image
+      f.input :title
+      f.input :publisher
+      f.input :year
+    end
+    f.actions
+  end
+end
