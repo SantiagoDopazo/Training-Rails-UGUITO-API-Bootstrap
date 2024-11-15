3555ead9ac118977b4c126cb72c17388ff85ca07
Santiago Agustin Dopazo <dopazosantiago18@gmail.com>
2024-11-15 10:19:42 -0300

    feat: [TR-596] Implemented Create Service for Note (#8)
    * feat implemention of a model Note so users can create multiple of them about the books they have
    
    * delete test to respect objective of the actual card
    
    * [200~fix(deps): restore Gemfile.lock to match master
    
    * fix(deps): restore Gemfile.lock to match master
    
    * fix: added method in  utility to return the content length of a note using Constant Hashes in each Utility
    
    * fix: changed note_type to enum and added null constraints
    
    * feat: change logic of content_length so its more delegate in Note model, added English and Spanish errors types
    
    * fix: added first argument in validate_review_word_limit method to fix no method error
    
    * feat: first test added
    
    * delete bybugs
    
    * upgrading tests and adding test to validate content_length methods
    
    * fix: content_length method to be more ruby style
    
    * fix: added last line in es.yml archive
    
    * feat: added more test for the content_length method
    
    * feat: added word_count test
    
    * change user possition in FactoryBot
    
    * feat: added show method with pagination
    
    * fix: change notes_filtered method to respect line length
    
    * feat: added show method
    
    * fix: wrong json in index and replace doble quotes with single quotes
    
    * feat: add a before_action to authenticate user before using index and show methods
    
    * feat: describe to index test
    
    * feat: test for show and index, added shared_examples to success responses
    
    * delete notes spec from request
    
    * rescue exceptiosn from invalid notes
    
    * finally controller rescue all posible exceptions
    
    * added test to note controller create service
    
    * delete do end to routes and added comment routes
    
    * Added scopes so the model has the responsability of filtering and render an error if the notetype is invalid
    
    * Change index if the note_type is invalid to render invalid_type
    
    * delete a contexte inside when user is not logged in because it was redundant
    
    * added compact method so scopes can be optional
    
    * reduce if sentence in index service for invalid note types
    
    * pruebas posteriores
    
    * feat: added context for valid and invalid note_types
    
    * fix: change validation in test so dont be exactly equal but has the same keys and size
    
    * fix: remove let serializer
    
    * feat:  added order test, upgrade note_type test
    
    * fix: rubocop
    
    * fix wrong condicional
    
    * fix corrections except doing get index common for all the test
    
    * fixing spec create note to use subject
    
    * added create to routes
    
    * fix get index and get show so it dosent repeat it in every context
    
    * unless fixed
    
    * fix wrong merge with TR-595
    
    * correct rubocop errors
    
    * change validation to be more simple and refactor methods to be more poliformic
    
    * fix test for creation to use change and added shared example
    
    * added test to prove that the note created is correct

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -3,6 +3,10 @@ module Api
     class NotesController < ApplicationController
       before_action :authenticate_user!
 
+      rescue_from ActiveRecord::RecordInvalid, with: :respond_invalid_record
+      rescue_from ActionController::ParameterMissing, with: :parameter_missing
+      before_action :validate_note_type_create, only: :create
+
       def index
         return render_invalid_note_type unless valid_note_type?
         render json: notes_filtered, status: :ok, each_serializer: IndexNoteSerializer
@@ -12,22 +16,15 @@ module Api
         render json: show_note, status: :ok, serializer: ShowNoteSerializer
       end
 
+      def create
+        current_user.notes.create!(note_params)
+        render json: { message: I18n.t('controller.note_create_success') }, status: :created
+      end
+
       private
 
       def notes
         current_user.notes
-      end
-
-      def valid_note_type?
-        !note_type_present? || note_type_ok?
-      end
-
-      def note_type_present?
-        params[:note_type].present?
-      end
-
-      def note_type_ok?
-        Note.note_types.keys.include?(params[:note_type])
       end
 
       def notes_filtered
@@ -41,8 +38,39 @@ module Api
         notes.find(params.require(:id))
       end
 
+      def note_params
+        params.require(:note).require(%i[title note_type content])
+        params.require(:note).permit(%i[title note_type content])
+      end
+
+      def valid_note_type?
+        params[:note_type].blank? || note_type_ok?(params[:note_type])
+      end
+
+      def validate_note_type_create
+        render_invalid_note_type unless note_type_ok?(note_params[:note_type])
+      end
+
+      def note_type_ok?(note_type)
+        Note.note_types.keys.include?(note_type)
+      end
+
+      def respond_invalid_record
+        render json: error_review_length, status: :bad_request
+      end
+
+      def parameter_missing
+        render json: { error: I18n.t('controller.note_parameter_missing') }, status: :bad_request
+      end
+
       def render_invalid_note_type
-        render json: { error: I18n.t(:error_note_type_invalid) }, status: :unprocessable_entity
+        render json: { error: I18n.t('controller.note_invalid_type') },
+               status: :unprocessable_entity
+      end
+
+      def error_review_length
+        { error: I18n.t('model.error_review_length',
+                        { limit: current_user.utility.short_content }) }
       end
     end
   end
